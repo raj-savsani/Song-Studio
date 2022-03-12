7c8c4c71ec4f80fc71982f1582b82f22ce399cde
raj-savsani <rrsavsani@gmail.com>
2022-03-12 09:55:57 +0530

    title changes , background animation added

--- song-studio/src/App.jsx
+++ song-studio/src/App.jsx
@@ -1,14 +1,20 @@
-
-import './App.css';
-import SongsList from './components/SongsList';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
+import "./App.css";
+import SongsList from "./components/SongsList";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
- 
   return (
     <div className="App">
       <Navbar />
+      <div class="ripple-background">
+        <div class="circle xxlarge shade1"></div>
+        <div class="circle xlarge shade2"></div>
+        <div class="circle large shade3"></div>
+        <div class="circle mediun shade4"></div>
+        <div class="circle small shade5"></div>
+      </div>
+
       <SongsList />
     </div>
   );
